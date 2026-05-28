f804524db3bb0eff6065bd7662549a4dc806eaa9
Andrii Bondar <andybondar3@gmail.com>
2026-05-28 16:17:04 +0100

    fix: for Vercel deployment

--- components/single-product/ProductRating.tsx
+++ components/single-product/ProductRating.tsx
@@ -1,18 +1,14 @@
 import { FaStar } from "react-icons/fa";
 
-async function ProductRating({ productId }: { productId: string }) {
+async function ProductRating() {
   const rating = 4.2;
   const count = 25;
 
   const className = `flex gap-1 items-center text-md mt-1 mb-4`;
   const countValue = `(${count}) reviews`;
 
-  const handleClick = () => {
-    console.log(productId);
-  };
-
   return (
-    <span className={className} onClick={handleClick}>
+    <span className={className}>
       <FaStar className="w-3 h-3" />
       {rating} {countValue}
     </span>
